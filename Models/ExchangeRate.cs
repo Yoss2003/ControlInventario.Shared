ba9497ae7af6a8e781cf06c5f77f52926d9c1c18
Alexandro-dev <mercadogarciaalexandro10@gmail.com>
2026-06-01 13:56:55 -0500

    Adición del tipo de cambio

---
+++ Models/ExchangeRate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ControlInventario.Shared.Models
+{
+    public class ExchangeRate
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string BaseCurrency { get; set; } = "USD"; // Siempre "USD" para SUNAT
+
+        [Required]
+        [StringLength(10)]
+        public string QuoteCurrency { get; set; } = "PEN"; // Siempre "PEN" para Perú (Soles)
+
+        [Required]
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal BuyPrice { get; set; } // Tipo de cambio: Compra (Ej: 3.7450)
+
+        [Required]
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal SellPrice { get; set; } // Tipo de cambio: Venta (Ej: 3.7550)
+
+        [Required]
+        public DateTime Date { get; set; } // Fecha oficial del tipo de cambio asignado por SUNAT
+    }
+}
