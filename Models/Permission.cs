6d0d9a54cb08e91099dc0df4c58d0b9881e1ada8
Alexandro-dev <mercadogarciaalexandro10@gmail.com>
2026-06-25 20:01:55 -0500

    Add permisos

---
+++ Models/Permission.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControlInventario.Shared.Models
+{
+    public class Permission
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty; // Ej: "Permitir Crear Artículos"
+
+        [Required]
+        [StringLength(50)]
+        public string SystemCode { get; set; } = string.Empty; // Ej: "CREATE_ARTICLES" (Este es el que leeremos en el código)
+    }
+}
