43f3adbd3cf064f7a1b6d6f2f6e57417a5a17747
Alexandro-dev <mercadogarciaalexandro10@gmail.com>
2026-06-26 09:07:55 -0500

    Nuevo usuario rol admin

--- Models/User.cs
+++ Models/User.cs
@@ -11,24 +11,28 @@ namespace ControlInventario.Shared.Models
 
         [Required] public string FirstName { get; set; } = string.Empty;
         [Required] public string LastName { get; set; } = string.Empty;
-        [Required] public string Email { get; set; } = string.Empty;
         [Required] public string Username { get; set; } = string.Empty;
-        [Required] public string Password { get; set; } = string.Empty; // Solo vive aquí
+        [Required] public string Password { get; set; } = string.Empty;
+        public string? Email { get; set; }
 
-        public int Age { get; set; }
-        public string BirthDate { get; set; } = string.Empty;
-        public string HireDate { get; set; } = string.Empty;
+        public int? Age { get; set; }
+        public string? BirthDate { get; set; } = string.Empty;
+        public string? HireDate { get; set; } = string.Empty;
 
-        // --- NUEVOS CAMPOS PREMIUM ---
-        public string PhoneNumber { get; set; } = string.Empty;
-        public string ProfilePictureUrl { get; set; } = string.Empty;
+        // CAMPOS RELACIONALES
+        public string? PhoneNumber { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+
+        // CAMPOS PARA GOOGLE AUTHENTICATOR
         public bool IsActive { get; set; } = true;
+        public string? TwoFactorSecret { get; set; }
+        public bool IsTwoFactorEnabled { get; set; }
 
-        // --- RELACIONES HACIA LAS TABLAS (Foreign Keys) ---
-        public int RoleId { get; set; }
+        // RELACIONES HACIA LAS TABLAS (Foreign Keys)
+        public int? RoleId { get; set; }
         public virtual Role? Role { get; set; }
-        public int JobPositionId { get; set; }
-        public int AreaId { get; set; }
-        public int ContractTypeId { get; set; }
+        public int? JobPositionId { get; set; }
+        public int? AreaId { get; set; }
+        public int? ContractTypeId { get; set; }
     }
 }
