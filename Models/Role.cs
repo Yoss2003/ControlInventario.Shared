9a806b1447c0e8d879d8a83bf094d3ae88d6b806
Alexandro-dev <mercadogarciaalexandro10@gmail.com>
2026-07-12 11:33:17 -0500

    Fix Errors

--- Models/Role.cs
+++ Models/Role.cs
@@ -4,7 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
 
-        public virtual ICollection<RolePermission>? RolePermissions { get; set; }
+        public List<RolePermission>? RolePermissions { get; set; }
     }
 }
